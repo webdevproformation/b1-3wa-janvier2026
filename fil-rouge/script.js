160462905a05897c314cdd944565e2ecafd681ac
Malik H <malik.h@webdevpro.net>
2026-01-05 16:29:00 +0100

    Update script.js

--- fil-rouge/script.js
+++ fil-rouge/script.js
@@ -2,6 +2,7 @@ function toggle()
 {
     document.querySelector("p").classList.toggle("d-none");
     alert("le bouton a été cliqué")
+    console.log("modifier de la visbilité de paragraphe")
 }
 
 document.querySelector(".js-toggle" , toggle)
